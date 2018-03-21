0aea9b376e190bbe696566d31122da871b862e7c
Dylan Stratton <dm.stratton94@gmail.com>
2018-03-21 11:46:00 -0500

    fix config

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
 'use strict';
-<<<<<<< HEAD
 exports.DATABASE_URL =
   process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
 exports.PORT = process.env.PORT || 8080;
-=======
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
-exports.PORT = process.env.PORT || 8080;
->>>>>>> bcbca716440857d1a62b1809ac7870d8dc8fa3f4
